c6f88e0a5b4c891598adfffc279f9d424ecbbad7
kms <kms@user-PC>
2019-03-12 11:10:39 +0900

    19/03/12 약관동의

---
+++ sboard/src/main/java/kr/co/sboard/service/MemberService.java
@@ -0,0 +1,12 @@
+package kr.co.sboard.service;
+
+import kr.co.sboard.vo.TermsVO;
+
+public interface MemberService {
+	
+	public void login();
+	public TermsVO terms();
+	public void register();
+
+
+}
